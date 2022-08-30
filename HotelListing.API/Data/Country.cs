d272a6c9553b709117d882f1bfbe3e13f9232e83
Sandra Kruska <sandra.kruska@tredition.de>
2022-08-30 20:03:37 +0200

    Vor Hinzufügen von SQL Express Connection Services

---
+++ HotelListing.API/Data/Country.cs
@@ -0,0 +1,14 @@
+﻿namespace HotelListing.API.Data
+{
+    public class Country
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string ShortName { get; set; }
+
+
+        // One-To-Many association with Hotel
+        public virtual IList<Hotel> Hotels { get; set; }
+
+    }
+}
